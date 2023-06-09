4d85be5bacfa5dfee8da7955386bca29330a2971
Abdulhalim Mabrouk Hafidh <50109942+amhafidhJr@users.noreply.github.com>
2023-06-09 16:42:58 +0300

    dashboard-designing-revamp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
